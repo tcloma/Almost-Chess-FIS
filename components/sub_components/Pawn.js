59da40ce45ffe4865d87301af68dd96549c3977d
Olivia <oliviatmanalastas@gmail.com>
2022-08-23 10:06:22 -0400

    Misc

--- components/sub_components/Pawn.js
+++ components/sub_components/Pawn.js
@@ -1,28 +1,24 @@
 import React from "react"
 
-// to inform each piece of its position
-// to give the piece 
-
-const Pawn = () => {
-
+const Pawn = ({xpos, ypos}) => {
    // const initialMovement = () => {
    //    // if the pawn is in starting position then it can move 1 or two spaces up
    //    if (xposition == '' && yposition == '' ) {
    //       // you can only move going up
    //       // either to go up one or to go up two spaces (only row 3 or row 4)
    //          // onClick it will show the posible movements 
-   //       // else you can't move it there
-         
+   //       // else you can't move it there   
    //    }
    // }
-
    // const pawnMoves = () => {
    //    // once pawn does initialMovement this is the only movement it can do
-
    // }
-   
+   const movePawn = ((e) => {
+      e.preventDefault()
+      
+   })
    return (
-      <p>p</p>
+      <p onClick={movePawn}> {xpos} x {ypos}</p>
    )
 }
 
