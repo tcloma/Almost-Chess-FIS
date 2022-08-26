e0fa5d867a90a16e73ec10b50ae830b46041dbf4
Olivia <oliviatmanalastas@gmail.com>
2022-08-25 20:54:18 -0400

    Delete method attempt

--- components/PieceLogCard.js
+++ components/PieceLogCard.js
@@ -3,12 +3,21 @@ import styles from '../styles/PieceLog.module.scss'
 
 const PieceLogCard = ({chessPiece}) => {
    const {x_pos , y_pos, piece_name} = chessPiece
+
+   const deletePiece = async (pieceId) => {
+      fetch(`http://localhost:9292/pieces/${pieceId}` , {
+         method: 'DELETE',
+      })
+      const data = await res.json(data)
+      fetchPieces()
+   }
+
    return (
       <div style={{ fontFamily: 'geneva', letterSpacing: '.2em', }} className={styles.pieceLogCardContainer}>
          <div className={styles.pieceLogCard}>
             <h1>{piece_name}</h1>
             <h1>{x_pos}x{y_pos}</h1>
-            <button onClick={handleDelete}>Delete</button>
+            <button onClick={deletePiece(piece.id)}>Delete</button>
          </div>
       </div>
    )
